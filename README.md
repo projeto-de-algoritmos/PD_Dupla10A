daf09fe8374dec5436596d937a9fe76a62bc2c1e
Wictorgirardi <wictor.girardi@gmail.com>
2020-12-04 18:40:58 -0300

    terminando algoritmo e prints

--- README.md
+++ README.md
@@ -8,10 +8,13 @@
 | 17/0039668  |  Lucas Ganda Carvalho  |
 | 17/0047326  |  Wictor Bastos Girardi |
 ## Sobre 
-Descreva os objetivos do seu projeto e como ele funciona. 
+O projeto busca resolver o problema de knapSack por meio do algoritmo de programação dinâmica.
 
 ## Screenshots
-Adicione 3 ou mais screenshots do projeto em funcionamento.
+![image example](./print1.png)
+![image example](./print2.png)
+![image example](./print3.png)
+![image example](./print4.png)
 
 ## Instalação 
 **Linguagem**: Phyton 3<br>
@@ -21,8 +24,3 @@ Para a execução do programa basta executar o comando no terminal:
 ```
 python3 list5.py
 ```
-
-
-
-
-
--- list5.py
+++ list5.py
@@ -1,66 +1,24 @@
-from collections import defaultdict 
-import os
-
-class Grafo: 
+def knapnapSacknap(W, wt, val, n): 
+    knap = [[0 for x in range(W + 1)] for x in range(n + 1)] 
   
-    def __init__(self, vertices): 
-        self.V = vertices  
-        self.Grafo = [] 
-   
-    def adicionarVertice(self, u, v, w): 
-        self.Grafo.append([u, v, w]) 
+    for i in range(n + 1): 
+        for w in range(W + 1): 
+            if i == 0 or w == 0: 
+                knap[i][w] = 0
+            elif wt[i-1] <= w: 
+                knap[i][w] = max(val[i-1] + knap[i-1][w-wt[i-1]],  knap[i-1][w]) 
+            else: 
+                knap[i][w] = knap[i-1][w] 
+  
+    return knap[n][W] 
 
+if __name__ == '__main__':
 
-    def printarVetor(self, dist): 
-        print("Distância de cada vertice é:") 
-        for i in range(self.V): 
-            print("% d \t\t % d" % (i, dist[i])) 
-      
-      
-    def BellmanFord(self, src): 
-        dist = [float("Inf")] * self.V 
-        dist[src] = 0 
-         
-        for i in range(self.V - 1): 
-            for u, v, w in self.Grafo: 
-                if dist[u] != float("Inf") and dist[u] + w < dist[v]: 
-                        dist[v] = dist[u] + w 
-       
-        for u, v, w in self.Grafo: 
-                if dist[u] != float("Inf") and dist[u] + w < dist[v]: 
-                        print ("Grafo contem um ciclo de distancias negativa")
-                        return
-                          
-        
-        self.printarVetor(dist) 
-
-cond = True
-qntVertice =int(input("Digite aqui quantos vértices tem o seu grafo:\n"))
-os.system('cls' if os.name == 'nt' else 'clear')
-
-
-g = Grafo(qntVertice) 
-
-
-while(cond):
-    print("informe agora o vertice inicial, o vértice final e o peso da aresta!\n")
-    print("Se não existir mais arestas escreva 'c' para calcular Bellman-Ford")
-
-    verticeInicial = input()
-
-    if(verticeInicial == 'c'):
-        cond = False 
-        os.system('cls' if os.name == 'nt' else 'clear')
-        continue
-
-    verticeFinal = input()
-    pesoAresta = input()
-    g.adicionarVertice(int(verticeInicial), int(verticeFinal), int(pesoAresta)) 
-    os.system('cls' if os.name == 'nt' else 'clear')
-
-
-
-os.system('cls' if os.name == 'nt' else 'clear')
-verticeReferencia = int(input("Informe agora o vertice de referência:"))
-
-g.BellmanFord(verticeReferencia)
+    print("Insira os valores de cada item os separando por espaços: ")
+    val = list(map(int, input().split()))
+    # val = [60, 100, 120]
+    print("Insira os peso de cada item os separando por espaços: ")
+    wt = list(map(int, input().split()))
+    W = int(input("Insira a capacidade da mochila: "))
+    n = len(val) 
+    print("A valor máximo que se pode carregar na mochila é: {}".format(knapnapSacknap(W, wt, val, n))) 
